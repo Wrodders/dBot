0795e4f57a20e9152e7f55530cb4524bf8da8fe9
Wrodders <winston.rodrigo.scott@gmail.com>
2023-12-29 09:42:43 -0300

    encoderInitial

---
+++ Firmware/drivers/encoder.h
@@ -0,0 +1,33 @@
+#ifndef ENCODER_H
+#define ENCODEr_H
+
+
+#include "../common/common.h"
+
+#define PI  3.14159
+
+typedef struct Encoder {
+    GPIO chA;
+    GPIO chB;
+    uint32_t cpr; // counts per revolution
+    uint32_t timPerif;
+    uint32_t count;
+    float *gRatio;
+}Encoder;
+
+
+
+static Encoder initEncoder(uint32_t timPerif, uint32_t pinA, uint32_t portA, uint32_t pinB, uint32_t portB ){
+    Encoder e;
+    e.chA.pin = pinA;
+    e.chA.port = portA;
+    e.chB.pin = pinB;
+    e.chB.pin = portB;
+    return e;
+}
+
+
+static uint16_t readEncoder(Encoder *e){
+
+}
+#endif // ENCODER_H
