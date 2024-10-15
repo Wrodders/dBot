9223c50ce7ef2b260b2c986da37f82e89f7a3ae0
Wrodders <winston.rodrigo.scott@gmail.com>
2024-10-15 22:12:40 +0100

    Reorg

--- Software/readSerial.py
+++ Software/readSerial.py
@@ -1,26 +1,50 @@
 import serial
 import time
+import zmq
+import threading
+from collections import namedtuple
 
-# Configure the serial port
-port = '/dev/serial0'  # Replace with your serial port (e.g., '/dev/ttyUSB0' for Linux)
-baudrate = 115200  # Set the baud rate (must match the device's setting)
+Topic = namedtuple('Topic', ['pub_id', 'name'])
 
-# Create a serial connection
-ser = serial.Serial(port, baudrate)
+pubMap = [
+    Topic('a', "CMD_RET"),
+    Topic('b', "ERROR"),
+    Topic('c', "INFO"),
+    Topic('d', "DEBUG"),
+    Topic('e', "IMU"),  # ROLL:PITCH:YAW
+    Topic('f', "ODOM", )  # LSPEED:RSPEED:TL:TR
+]
 
-# Give some time for the connection to establish
-time.sleep(2)
+def get_topic_by_id(pub_id):
+    for topic in pubMap:
+        if topic.pub_id == pub_id:
+            return topic.name
+    return None
 
-try:
+def serial_reader(ser, zmq_socket):
     while True:
-        if ser.in_waiting > 0:  # Check if there is data to read
-            line = ser.readline()  # Read a line of data
+        if ser.readable():
             try:
-                print(line.decode('utf-8').strip())  # Print the decoded line
+                msg_frame = ser.readline()
+                msg = msg_frame.decode('utf-8').rstrip("\n")
+                msg_id = msg[1]
+                msg_data = msg[2:]
+                topic = get_topic_by_id(msg_id)
+                if topic:
+                    zmq_socket.send_multipart([topic.encode(), msg_data.encode()])
             except Exception as e:
-                print(e)
-except KeyboardInterrupt:
-    print("Exiting...")
+                print(f"Serial read error: {e}")
 
-finally:
-    ser.close()  # Close the serial port
+def start_serial_reader(ser_port, zmq_socket):
+    ser = serial.Serial(ser_port, 115200)
+    threading.Thread(target=serial_reader, args=(ser, zmq_socket)).start()
+
+def setup_zmq_publisher():
+    context = zmq.Context()
+    zmq_socket = context.socket(zmq.PUB)
+    zmq_socket.bind("tcp://*:5555")  # Publishing on port 5555
+    return zmq_socket
+
+if __name__ == "__main__":
+    zmq_socket = setup_zmq_publisher()
+    start_serial_reader('/dev/serial0', zmq_socket)
