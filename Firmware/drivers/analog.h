781a9a36ec46296c8b490ade01a3e83304a81180
Wrodders <winston.rodrigo.scott@gmail.com>
2025-02-02 15:34:47 +0000

    update

---
+++ Firmware/drivers/analog.h
@@ -0,0 +1,10 @@
+#ifndef ANALOG_H
+#define ANALOG_H
+
+#include <libopencm3/stm32/adc.h>
+#include <libopencm3/stm32/dma.h>
+#include <libopencm3/stm32/timer.h>
+
+#include "../common/common.h"
+
+#endif // ANALOG_H
